bd44fa57ecb54fa2468b9c89a1ffe2f047806587
Anutka_bestiya <anchikfyz@yandex.ru>
2023-02-26 19:03:40 +0500

    style(button)feat(text)

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,37 @@
 console.log('Hello');
+
+const editPopup = document.querySelector('.edit-popup');
+if (!editPopup) {
+  throw new Error('No editPopup');
+}
+
+const editProfileOpenButton = document.querySelector('.profile__edit-button');
+if (!editProfileOpenButton) {
+  throw new Error('No editProfileOpenButton');
+}
+
+const editProfileCloseButton = document.querySelector('.profile__close-button');
+if (!editProfileCloseButton) {
+  throw new Error('No editProfileCloseButton');
+}
+
+editProfileOpenButton.addEventListener('click', function () {
+  openEditPopup(editPopup);
+});
+editProfileCloseButton.addEventListener('click', function () {
+  closeEditPopup(editPopup);
+});
+function openEditPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closeEditPoput(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+const userName = 'Жак-Ив Кусто';
+const userAbout = 'Исследователь океана';
+const userNameElement = document.querySelector('.user__name');
+userNameElement.textContent = userName;
+const userAboutElement = document.querySelector('.user__about');
+userAboutElement.textContent = userAbout;
