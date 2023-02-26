d79b7b4edafde1ce0271205b86751cb77c1f194e
Anutka_bestiya <anchikfyz@yandex.ru>
2023-02-27 01:54:00 +0500

    feat(saveText)

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,76 @@
 console.log('Hello');
-
+//находим кнопку форму радактирования данных=попап
 const editPopup = document.querySelector('.edit-popup');
 if (!editPopup) {
   throw new Error('No editPopup');
 }
-
+//находим кнопку редактировать попап
 const editProfileOpenButton = document.querySelector('.profile__edit-button');
 if (!editProfileOpenButton) {
   throw new Error('No editProfileOpenButton');
 }
-
+//находим кнопку закрыть попап
 const editProfileCloseButton = document.querySelector('.profile__close-button');
 if (!editProfileCloseButton) {
   throw new Error('No editProfileCloseButton');
 }
-
+//события клика на открытие/закрытие попапа + функции
 editProfileOpenButton.addEventListener('click', function () {
   openEditPopup(editPopup);
 });
 editProfileCloseButton.addEventListener('click', function () {
   closeEditPopup(editPopup);
 });
-function openEditPopup(popup) {
-  popup.classList.add('popup_opened');
+function openEditPopup() {
+  editPopup.classList.add('popup_opened');
+}
+function closeEditPopup() {
+  editPopup.classList.remove('popup_opened');
 }
 
-function closeEditPoput(popup) {
-  popup.classList.remove('popup_opened');
+let formElement = document.querySelector('.form-edit');
+console.log(formElement);
+let nameInput = formElement.querySelector('.form-user__name');
+console.log(nameInput);
+let aboutInput = formElement.querySelector('.form-user__about');
+console.log(aboutInput);
+if (!nameInput) {
+  throw new Error('No nameInput');
 }
+if (!aboutInput) {
+  throw new Error('No aboutInput');
+}
+nameInput.getAttribute('value');
+console.log(nameInput.value);
+aboutInput.getAttribute('value');
+console.log(aboutInput.value);
 
-const userName = 'Жак-Ив Кусто';
-const userAbout = 'Исследователь океана';
-const userNameElement = document.querySelector('.user__name');
-userNameElement.textContent = userName;
-const userAboutElement = document.querySelector('.user__about');
-userAboutElement.textContent = userAbout;
+let userNameElement = document.querySelector('.user__name');
+let userAboutElement = document.querySelector('.user__about');
+if (!userNameElement) {
+  throw new Error('No userNameElement');
+}
+if (!userAboutElement) {
+  throw new Error('No userAboutElement');
+}
+userNameElement.textContent = nameInput.value;
+userAboutElement.textContent = aboutInput.value;
+console.log(userNameElement.textContent);
+console.log(userAboutElement.textContent);
+
+formElement.addEventListener('submit', handleFormSubmit);
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  userName = nameInput.value;
+  console.log(userName);
+  userAbout = aboutInput.value;
+  console.log(userAbout);
+
+  userNameElement.textContent = userName;
+  userAboutElement.textContent = userAbout;
+  console.log(userNameElement.textContent);
+  console.log(userAboutElement.textContent);
+
+  closeEditPopup(editPopup);
+}
